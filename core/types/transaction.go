9c204d0cd17440fdab165915259ca864e674808a
rtuna <artur.hnat@gmail.com>
2026-03-19 14:34:46 +0100

    core/types: add Optimism deposit transaction (type 0x7E) support
    Add DepositTx struct and marshalling for Optimism deposit transactions.
    These are system transactions with no signature (v/r/s must be zero),
    used for L1-to-L2 message passing. Also registers type 0xFF as an
    alternative deposit type for ZKSync compatibility.

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -212,6 +212,8 @@ func (tx *Transaction) decodeTyped(b []byte) (TxData, error) {
 		inner = new(BlobTx)
 	case SetCodeTxType:
 		inner = new(SetCodeTx)
+	case DepositTxType:
+		inner = new(DepositTx)
 	default:
 		return nil, ErrTxTypeNotSupported
 	}
